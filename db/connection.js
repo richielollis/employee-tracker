4b82d9bf03d99e1ef503e824205410adeee1bc7e
Richie Lollis <richiedlollis@gmail.com>
2022-10-14 15:45:39 -0400

    database setup with tables, server up and running, and connection to mysql made

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'rootroot',
+        database: 'employee_tracker'
+    },
+    console.log('Connected to the employee_tracker databse.')
+);
+
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
+
