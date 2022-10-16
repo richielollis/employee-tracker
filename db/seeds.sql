52a6c022734782c5d52df041fd71815d55c413e1
Richie Lollis <richiedlollis@gmail.com>
2022-10-16 10:32:45 -0400

    commit for clone to home cpu

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO department (name) 
+VALUES
+    ('Engineering'),
+    ('Finacne'),
+    ('Legal'),
+    ('Sales');
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('./db/connection');
+const inquirer = require('inquirer');
 // const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
